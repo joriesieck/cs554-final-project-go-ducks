2f4bf1d7aa83ec15f6ae7e5071944702196bfc07
Jack Brummer <jbrummer@stevens.edu>
2021-11-17 16:38:45 -0500

    Setting up initial layouts for question rendering

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,35 @@
-export default function QuestionPage({ questionTitle }) {
-  return <div></div>;
+/*
+import layout things
+*/
+import Head from "next/head";
+
+export default function Question({ questionData }) {
+  return (
+    <div>
+      <Head>
+        <title>{questionData.title}</title>
+      </Head>
+      <article>
+        <p>{questionData.answer}</p>
+      </article>
+    </div>
+  );
 }
 
-export async function getStaticProps() {}
+export async function getStaticPaths() {
+  const paths = getAllQuestionIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  // Make calls to API
+  const questionData = await getQuestionData(params.id);
+  return {
+    props: {
+      questionData,
+    },
+  };
+}
