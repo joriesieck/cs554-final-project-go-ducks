0fdd651173b331495dcbb3946d3228e479588fc1
Jorie Sieck <jsieck@stevens.edu>
2021-11-19 14:44:21 -0500

    added error checks

--- src/components/CreateUser/CreateUser.js
+++ src/components/CreateUser/CreateUser.js
@@ -1,13 +1,48 @@
 import { auth } from "../../firebase/firebaseSetup";
 import { createUserWithEmailAndPassword } from "@firebase/auth";
-import { Button, TextField } from "@mui/material";
+import { Alert, Button, TextField } from "@mui/material";
+import { useState } from "react";
+
+// TODO styling
 
 export default function CreateUser() {
+	const [errors, setErrors] = useState(null);
+
 	const createUser = async (e) => {
 		e.preventDefault();
+		setErrors(null);
 
-		const email = document.getElementById('email').value;
-		const password = document.getElementById('password').value;
+		let username = e.target[0].value;
+		let email = e.target[2].value;
+		let password = e.target[4].value;
+
+		// make sure all values exist
+		const errorList = [];
+		if (!username) errorList.push('Please enter a username.');
+		if (!email) errorList.push('Please enter an email.');
+		if (!password) errorList.push('Please enter a password.');
+		// make sure all values are strings
+		if (username && typeof username !== 'string') errorList.push('Username must be a string.');
+		if (email && typeof email !== 'string') errorList.push('Email must be a string.');
+		if (password && typeof password !== 'string') errorList.push('Password must be a string.');
+		// trim and make sure all values are nonempty
+		username = username.trim();
+		email = email.trim();
+		// don't trim password
+		if (username==='') errorList.push('Username must contain at least one character.');
+		if (email==='') errorList.push('Email must contain at least one character.');
+		if (password==='') errorList.push('Password must contain at least one character.');
+		// make sure password is at least 6 characters
+		if (password.length<6) errorList.push('Password must be at least 6 characters.');
+
+		// if there were errors, set errors
+		if (errorList.length>0) {
+			setErrors(errorList);
+			return;
+		}
+
+		// TODO make sure user doesn't exist
+
 
 		let result;
 		try {
@@ -16,6 +51,7 @@ export default function CreateUser() {
 			console.log(result);
 		} catch (e) {
 			console.log(e);
+			setErrors([e.toString()]);
 		}
 	}
 
@@ -23,12 +59,17 @@ export default function CreateUser() {
 		<>
 			<h1>Create User</h1>
 			<form onSubmit={createUser}>
-				<label htmlFor="email">Email</label>
-				<TextField id="email" required type="email" />
-				<label htmlFor="password">Password</label>
-				<TextField id="password" required type="password" />
+				<TextField id="username" required label="Username" />
+				<TextField id="email" required type="email" label="Email" />
+				<TextField id="password" required type="password" label="Password" helperText="Must be at least 6 characters." />
 				<Button type="submit">Create User</Button>
 			</form>
+
+			{errors && <Alert severity="error">
+				<ul>
+					{errors.map((error) => <li key={error}>{error}</li>)}
+				</ul>
+			</Alert>}
 		</>
 	)
 }
