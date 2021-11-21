5d9006486d66955f2e45a455a160e24b663747f5
Jorie Sieck <jsieck@stevens.edu>
2021-11-21 08:28:19 -0500

    route for getUser

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,14 +3,36 @@ const router = express.Router();
 const data = require('../data');
 const userData = data.users;
 
+// get user
 router.get('/:username', async (req, res) => {
+  // get the username from req.params
+  let username = req.params.username;
+  // make sure it's a string, nonempty, etc
   try {
-    const user = await userData.getUserByName(req.params.username);
-    res.json(user);
+    if (!username) throw 'Please provide a username.';
+    if (typeof username!=='string') throw 'Username must be a string.';
+    username = username.trim();
+    if (username==='') throw 'Username must contain at least one character.';
+    if (username.match(/[   ]/)) throw 'Username cannot contain any whitespace characters';
   } catch (e) {
-    res.status(404).json({ error: e.message });
+    res.status(400).json({ error: e });
     return;
   }
+
+  // get the user
+  let user;
+  try {
+    user = await userData.getUserByName(username);
+    if (!user.name) throw 'No user found.';
+  } catch (e) {
+    res.status(404).json({ error: e.message || e.toString() });
+    return;
+  }
+
+  // send back to front end
+  res.json(user);
 });
+
+// create user - TODO once that fn is finished
 
 module.exports = router;
