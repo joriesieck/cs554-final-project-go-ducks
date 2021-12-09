c3dbcc4900ad748307f0be73e6cea004a9329a4a
Jack Brummer <jbrummer@stevens.edu>
2021-12-08 19:17:58 -0500

    Rendering App in index file

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+
+import CreateUser from './components/CreateUser/CreateUser';
+import LogIn from './components/LogIn/LogIn';
+
+function App() {
+  return (
+    <>
+      <CreateUser />
+      <LogIn />
+    </>
+  );
+}
+export default App;
