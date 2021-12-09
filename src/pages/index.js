3476b9a2670849bb73e2e5189026b8c89f4974ae
Jack Brummer <jbrummer@stevens.edu>
2021-12-08 19:18:47 -0500

    Rendering App in index

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+/*
+import layout things
+*/
+import Head from 'next/head';
+import CreateUser from '../components/CreateUser/CreateUser';
+import LogIn from '../components/LogIn/LogIn';
+import React from 'react';
+import App from '../App';
+
+const Index = () => {
+  return <App />;
+};
+
+export default Index;
