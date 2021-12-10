91c8882afc1657bad4c022f413557058ec708ddf
Jack Brummer <jbrummer@stevens.edu>
2021-12-09 21:50:26 -0500

    firebase process.env change

--- client/src/firebase/firebaseSetup.js
+++ client/src/firebase/firebaseSetup.js
@@ -1,15 +1,14 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider, FacebookAuthProvider, GithubAuthProvider } from 'firebase/auth';
-import env from 'react-dotenv';
+import { getAuth } from 'firebase/auth';
 
 // add our API keys
 const firebaseConfig = {
-	apiKey: env.FIREBASE_API_KEY,
-	authDomain: env.FIREBASE_AUTH_DOMAIN,
-	projectId: env.FIREBASE_PROJECT_ID,
-	storageBucket: env.FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: env.FIREBASE_SENDER_ID,
-	appId: env.FIREBASE_APP_ID
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // initialize the app
