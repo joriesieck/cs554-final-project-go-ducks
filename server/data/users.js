ff8c1cb0df1d59f0fb71b99ca5af8ec807ef23d8
Elias Frieling <eliasfrieling@gmail.com>
2021-12-10 17:47:18 -0500

    Added data checks

--- server/data/users.js
+++ server/data/users.js
@@ -1,20 +1,25 @@
 const mongoCollections = require('../config/mongoCollections');
+const { checkString } = require('../inputChecks');
 const users = mongoCollections.users;
 
 const exportedMethods = {
   async getUserByName(username) {
+    checkString(username, 'Username', false);
     const userCollection = await users();
     const user = await userCollection.findOne({ username: username });
     if (!user) throw `User with username ${username} not found`;
     return user;
   },
   async getUserByEmail(email) {
+    checkString(email, 'email', false);
     const userCollection = await users();
     const user = await userCollection.findOne({ email: email });
     if (!user) throw `User with email ${email} not found`;
     return user;
   },
   async doesUserExist(username, email) {
+    checkString(username, 'Username', false);
+    checkString(email, 'email', false);
     const userCollection = await users();
     const userByUsername = await userCollection.findOne({ username: username });
     if (userByUsername) throw `User with username ${username} already exists`;
@@ -22,8 +27,9 @@ const exportedMethods = {
     if (userByEmail) throw `User with email ${email} already exists`;
   },
   async addUser(username, email, optedForLeaderboard) {
+    checkString(username, 'Username', false);
+    checkString(email, 'email', false);
     await this.doesUserExist(username, email);
-
     const userCollection = await users();
     const insertInfo = await userCollection.insertOne({
       username: username,
