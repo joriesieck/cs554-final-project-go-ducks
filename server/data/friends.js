ed1ddb7ebcdea82e2f4ed5d948fd9a526ce954a5
asengstrom <aengstro@stevens.edu>
2021-12-11 15:30:37 -0500

    get friend

--- server/data/friends.js
+++ server/data/friends.js
@@ -1,14 +1,55 @@
 const mongoCollections = require('../config/mongoCollections');
-
+const { checkObjId } = require('../inputChecks');
 const users = mongoCollections.users;
 
+
+// get friend
+async function getFriendById(userId, friendId){
+    // check Ids, assuming input as string versions of ids
+    checkObjId(userId);
+    checkObjId(friendId);
+
+    // ids as actual objId
+    let uid = ObjectId(userId);
+    let fid = ObjectId(friendId);
+
+    const userCollection = await users();
+    // get user
+    const user = await userCollection.findOne({_id: uid});
+    if (!user){
+        throw `User with ID ${userId} not found`;
+    }
+
+    // get friend
+    const [ friend ] = user.friends.filter(e => e[0] === fid);
+    if (!friend){
+        throw  `User with ID ${userId} has no friend with ID ${friendId}`;
+    }
+
+    return friend;
+}
+
+// get all friends
+async function getAllFriends(userId){
+
+}
+
+// should there be get pending friend and get all pending as well? I'll implement
+
 // add friend by id -> add to pending array
-// pending array will fn as obj array in from [ {id: 'sent'}, {id: 'received'}] differentiates 
+// pending array will fn as 2d array in form [ [id: 'sent'], [id: 'received']] differentiates
 // whether request needs approval from given user 
+// could also refactor this as instead of single pending array, one sent array and one received array
 async function addFriend(requesterId, requesteeId){
     // check ids
+    checkObjId(requesterId, 'Requester Id');
+    checkObjId(requesteeId, "Requestee Id");
 
-    // check 
+    // check if friend already in pending
+    
+
+    // if yes, check if request sent or received, if received then accept friend
+
     // update pending arrays with relevant info
 
 }
@@ -26,14 +67,4 @@ async function removeFriend(userId, friendId){
 // invite friend to game?
 async function inviteFriend(userId, friendId){
 
-}
-
-// get friend
-async function getFriendById(userId, friendId){
-
-}
-
-// get all friends
-async function getAllFriends(userId){
-    
 }
