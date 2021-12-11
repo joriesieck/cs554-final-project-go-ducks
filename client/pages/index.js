ee398b7c496af11f02c848bfcc4ad1d02368eeee
Jack Brummer <jbrummer@stevens.edu>
2021-12-11 16:12:16 -0500

    Fixed index

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,33 +1,8 @@
-/*
-import layout things
-*/
-import Head from "next/head";
+import React from "react";
+import App from "../src/App";
 
-export default function Home({ questionData }) {
-  return (
-    <div>
-      <title>Jeopardy</title>
-    </div>
-  );
-}
+const Index = () => {
+  return <App />;
+};
 
-// Won't be necessary due to server rendering
-
-// export async function getStaticPaths() {
-//   const paths = getAllQuestionIds();
-//   return {
-//     paths,
-//     fallback: false,
-//   };
-// }
-
-//
-export async function getServerSideProps(context) {
-  // Make calls to API
-  const questionData = await getQuestionData(context.id);
-  return {
-    props: {
-      questionData,
-    },
-  };
-}
+export default Index;
