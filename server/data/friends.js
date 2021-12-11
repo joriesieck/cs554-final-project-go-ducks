9f6598b55fa0461b9c55fb473fc5306102fb6b9d
asengstrom <aengstro@stevens.edu>
2021-12-11 15:49:42 -0500

    making corrections to get friend

--- server/data/friends.js
+++ server/data/friends.js
@@ -2,7 +2,6 @@ const mongoCollections = require('../config/mongoCollections');
 const { checkObjId } = require('../inputChecks');
 const users = mongoCollections.users;
 
-
 // get friend
 async function getFriendById(userId, friendId){
     // check Ids, assuming input as string versions of ids
@@ -20,21 +19,41 @@ async function getFriendById(userId, friendId){
         throw `User with ID ${userId} not found`;
     }
 
-    // get friend
-    const [ friend ] = user.friends.filter(e => e[0] === fid);
-    if (!friend){
+    // check if friend in friend array
+    const [ inFriends ] = user.friends.filter(e => e[0] === fid);
+    if (!inFriends){
         throw  `User with ID ${userId} has no friend with ID ${friendId}`;
     }
+
+    // get user by id, not implemented in user file, will just implement here for now
+
 
     return friend;
 }
 
 // get all friends
 async function getAllFriends(userId){
+    // check Ids, assuming input as string versions of ids
+    checkObjId(userId);
+    checkObjId(friendId);
+
+    // ids as actual objId
+    let uid = ObjectId(userId);
+    let fid = ObjectId(friendId);
+
+    const userCollection = await users();
+    // get user
+    const user = await userCollection.findOne({_id: uid});
+    if (!user){
+        throw `User with ID ${userId} not found`;
+    }
+
+    // grab all friends
+    const friends = user.friends.map(e => e[0])
 
 }
 
-// should there be get pending friend and get all pending as well? I'll implement
+// should there be get pending friend and get all pending as well? might as well
 
 // add friend by id -> add to pending array
 // pending array will fn as 2d array in form [ [id: 'sent'], [id: 'received']] differentiates
@@ -66,5 +85,9 @@ async function removeFriend(userId, friendId){
 
 // invite friend to game?
 async function inviteFriend(userId, friendId){
+
+}
+
+module.exports = {
 
 }
