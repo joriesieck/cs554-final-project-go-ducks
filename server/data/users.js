9d7ffb2ce85236d1ce9f6ddae369fab2b163c565
Elias Frieling <eliasfrieling@gmail.com>
2021-12-12 13:26:48 -0500

    Added edit user functions and route

--- server/data/users.js
+++ server/data/users.js
@@ -1,5 +1,5 @@
 const mongoCollections = require('../config/mongoCollections');
-const { checkString } = require('../inputChecks');
+const { checkString, checkBool } = require('../inputChecks');
 const users = mongoCollections.users;
 
 const exportedMethods = {
@@ -29,6 +29,7 @@ const exportedMethods = {
   async addUser(username, email, optedForLeaderboard) {
     checkString(username, 'Username', false);
     checkString(email, 'email', false);
+    checkBool(optedForLeaderboard, 'optedForLeaderboard');
     await this.doesUserExist(username, email);
     const userCollection = await users();
     const insertInfo = await userCollection.insertOne({
@@ -40,6 +41,35 @@ const exportedMethods = {
       optedForLeaderboard: optedForLeaderboard,
     });
     return insertInfo;
+  },
+  async updateUser(originalEmail, updatedFields) {
+    const { username, email, optedForLeaderboard } = updatedFields;
+    console.log(username);
+    console.log(email);
+    console.log(optedForLeaderboard);
+    const userCollection = await users();
+    if (username !== undefined) {
+      //If new username is supplied, check its valid and does not exist elsewhere
+      checkString(username, 'Username', false);
+      const userByUsername = await userCollection.findOne({
+        username: username,
+      });
+      if (userByUsername) throw `User with username ${username} already exists`;
+    }
+    if (email !== undefined) {
+      //If new email is supplied, check its valid and does not exist elsewhere
+      checkString(email, 'email', false);
+      const userByEmail = await userCollection.findOne({ email: email });
+      if (userByEmail) throw `User with email ${email} already exists`;
+    }
+    if (optedForLeaderboard !== undefined) {
+      checkBool(optedForLeaderboard, 'optedForLeaderboard');
+    }
+    await userCollection.updateOne(
+      { email: originalEmail },
+      { $set: updatedFields }
+    );
+    return await this.getUserByEmail(email ?? originalEmail);
   },
 };
 module.exports = exportedMethods;
--- server/routes/users.js
+++ server/routes/users.js
@@ -84,7 +84,43 @@ router.post('/', async (req, res) => {
   // send the new user to the front end
   res.status(201).json(user);
 });
-
+router.patch('/edit-user', async (req, res) => {
+  let { originalEmail, username, newEmail, optedForLeaderboard } = req.body;
+  //originalEmail will be used to find the user we are updating
+  let updatedFields = {};
+  try {
+    const user = await userData.getUserByEmail(originalEmail);
+    if (username !== undefined && username !== user.username) {
+      username = checkString(username, 'Username', false);
+      updatedFields.username = username;
+    }
+    if (newEmail !== undefined && newEmail !== user.email) {
+      email = checkString(newEmail, 'Email', false);
+      updatedFields.email = email;
+    }
+    if (
+      optedForLeaderboard !== undefined &&
+      optedForLeaderboard !== user.optedForLeaderboard
+    ) {
+      checkBool(optedForLeaderboard, 'optedForLeaderboard');
+      updatedFields.optedForLeaderboard = optedForLeaderboard;
+    }
+  } catch (e) {
+    res.status(400).json({ error: `Error in request: ${e}` });
+    return;
+  }
+  if (Object.keys(updatedFields).length === 0) {
+    //No fields updated
+    res.status(400).json({ error: 'Minimum of one field must be updated' });
+  } else {
+    try {
+      updatedUser = await userData.updateUser(originalEmail, updatedFields);
+      res.status(201).json(updatedUser);
+    } catch (e) {
+      res.status(400).json({ error: `Could not update user. Error: ${e}` });
+    }
+  }
+});
 // remove user
 router.delete('/:username', async (req, res) => {
   // get the username from req.params
