8d84c88c593b5e213313f13bbaad1c8c0b9d5b70
Jack Brummer <jbrummer@stevens.edu>
2021-12-13 18:28:10 -0500

    Categories and questions data shows

--- client/pages/categories/[id].js
+++ client/pages/categories/[id].js
@@ -1,24 +1,30 @@
 import Head from "next/head";
-import { Button } from '@mui/material'
+import { Button } from "@mui/material";
+import Link from "next/link";
 
 import axios from "axios";
 
 const baseUrl = "http://jservice.io/api";
 
-export default function CategoryPage({ categoryData }) {
+export default function CategoryPage({ categoryData, children }) {
   return (
     <div>
-      <Head>
-        <h1>
-          Category: {categoryData.title}
-        </h1>
-      </Head>
+      <h1>Category: {categoryData.title}</h1>
+      <div>{children}</div>
       <div>
         <Button>Random question</Button>
         <Button>Practice Sequence</Button>
         <ul>
-          {categoryData.clues.map((question) => {
-            return <li>{question.question}</li>
+          {categoryData.clues.map((question, index) => {
+            return (
+              <li key={question.id}>
+                <Link
+                  href={`/questions/${question.id}?category=${question.category_id}&index=${index}`}
+                >
+                  <a>{question.question}</a>
+                </Link>
+              </li>
+            );
           })}
         </ul>
       </div>
@@ -35,9 +41,9 @@ export async function getServerSideProps({ params }) {
   let categoryData = await getCategories(params.id);
 
   categoryData.clues = categoryData.clues.filter((question) => {
-    return (question.question !== "")
-  })
-  
+    return question.question !== "";
+  });
+
   return {
     props: {
       categoryData,
