d518099278e94bf1926ef48f7b7756f1e36bcc46
Jack Brummer <jbrummer@stevens.edu>
2021-12-14 21:41:06 -0500

    react router alongside next

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,11 +1,9 @@
 import './global.css';
 import App from '../App';
-import AuthContainer from '../AuthContainer';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from '../redux/store';
 import { BrowserRouter as Router } from 'react-router-dom';
-import NavBar from '../components/Nav/Nav';
 
 export default function _app({ Component, pageProps }) {
   return (
