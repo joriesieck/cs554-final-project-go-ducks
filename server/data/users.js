0017394d282d6a0eaea450fb237c4eb2176f0c5c
Elias Frieling <eliasfrieling@gmail.com>
2021-12-15 10:07:52 -0500

    Moved caching from server to route

--- server/data/users.js
+++ server/data/users.js
@@ -7,12 +7,6 @@ const {
   checkEmail,
 } = require('../inputChecks');
 const users = mongoCollections.users;
-const bluebird = require('bluebird');
-const redis = require('redis');
-const client = redis.createClient();
-
-bluebird.promisifyAll(redis.RedisClient.prototype);
-bluebird.promisifyAll(redis.Multi.prototype);
 
 async function removeFriendAll(friendName) {
   checkString(friendName, 'Friend Name', false);
@@ -37,56 +31,25 @@ async function removeFriendAll(friendName) {
 const exportedMethods = {
   async getUserById(id) {
     checkObjId(id, 'User ID');
-    const cachedUser = await client.hgetAsync('idCache', id);
-    if (cachedUser) {
-      return JSON.parse(cachedUser);
-    } else {
-      const parsedId = ObjectId(id);
-      const userCollection = await users();
-      const user = await userCollection.findOne({ _id: parsedId });
-      if (!user) throw `User with ID ${id} not found`;
-      await client.hmsetAsync('idCache', id, JSON.stringify(user));
-      return user;
-    }
+    const parsedId = ObjectId(id);
+    const userCollection = await users();
+    const user = await userCollection.findOne({ _id: parsedId });
+    if (!user) throw `User with ID ${id} not found`;
+    return user;
   },
   async getUserByName(username) {
     checkString(username, 'Username', false);
-    const cachedUserID = await client.hgetAsync('usernameCache', username); //returns an ID
-    if (cachedUserID) {
-      const cachedUser = await client.hgetAsync('idCache', cachedUserID); //returns all information
-      return JSON.parse(cachedUser);
-    } else {
-      const userCollection = await users();
-      const user = await userCollection.findOne({ username: username });
-      if (!user) throw `User with username ${username} not found`;
-      await client.hmsetAsync('usernameCache', username, user._id.toString());
-      await client.hmsetAsync(
-        'idCache',
-        user._id.toString(),
-        JSON.stringify(user)
-      );
-      return user;
-    }
+    const userCollection = await users();
+    const user = await userCollection.findOne({ username: username });
+    if (!user) throw `User with username ${username} not found`;
+    return user;
   },
   async getUserByEmail(email) {
     checkEmail(email, 'Email');
-    const cachedUserID = await client.hgetAsync('emailCache', email); //returns an ID
-    if (cachedUserID) {
-      const cachedUser = await client.hgetAsync('idCache', cachedUserID); //returns all information
-      return JSON.parse(cachedUser);
-    } else {
-      const userCollection = await users();
-      const user = await userCollection.findOne({ email: email });
-      if (!user) throw `User with email ${email} not found`;
-      await client.hmsetAsync('emailCache', email, user._id.toString());
-      await client.hmsetAsync(
-        'idCache',
-        user._id.toString(),
-        JSON.stringify(user)
-      );
-
-      return user;
-    }
+    const userCollection = await users();
+    const user = await userCollection.findOne({ email: email });
+    if (!user) throw `User with email ${email} not found`;
+    return user;
   },
   async doesUserExist(username, email) {
     checkString(username, 'Username', false);
@@ -137,21 +100,7 @@ const exportedMethods = {
       { email: originalEmail },
       { $set: updatedFields }
     );
-    await client.hdelAsync('emailCache', originalEmail); //If the user is stored in the cache, we need to make sure we dont get the old version from cache
     const updatedUser = await this.getUserByEmail(email ?? originalEmail);
-
-    //updates or sets usernameCache with the current username value
-    await client.hmsetAsync(
-      'usernameCache',
-      updatedUser.username,
-      updatedUser._id.toString()
-    );
-    // if the user was cached, the cache with update with new values, otherwise it will just be added to the cache
-    await client.hmsetAsync(
-      'idCache',
-      updatedUser._id.toString(),
-      JSON.stringify(updatedUser)
-    );
     return updatedUser;
   },
   async removeUser(username) {
@@ -159,10 +108,8 @@ const exportedMethods = {
     const userCollection = await users();
     const userByUsername = await userCollection.findOne({ username: username });
     if (!userByUsername) throw `User with username ${username} doesn't exist`;
-    await removeFriendAll(username);
-    await client.hdelAsync('idCache', userByUsername._id.toString());
-    await client.hdelAsync('usernameCache', userByUsername.username.toString());
-    await client.hdelAsync('emailCache', userByUsername.email.toString());
+    //await removeFriendAll(username);
+
     return await userCollection.deleteOne({ username: username });
   },
 };
