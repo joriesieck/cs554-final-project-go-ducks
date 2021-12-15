492484461c005937140b748d94619cebee892e3a
Jack Brummer <jbrummer@stevens.edu>
2021-12-14 21:41:06 -0500

    react router alongside next

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,6 +1,5 @@
 import './global.css';
 import App from '../App';
-import AuthContainer from '../AuthContainer';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from '../redux/store';
