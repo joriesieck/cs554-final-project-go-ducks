8f5d6e9fb836e4befd454eb61f2a6960bb77a0a0
asengstrom <aengstro@stevens.edu>
2021-12-17 02:00:31 -0500

    search result modal added but it's weird

--- client/src/components/Friends/Friends.js
+++ client/src/components/Friends/Friends.js
@@ -1,8 +1,10 @@
 import {useState, useEffect} from 'react';
 import { useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import { checkString } from '../../utils/inputChecks';
 import {
     getUserByEmail,
+    searchUsersByName,
     addFriend,
     removeFriend, 
     removePendingFriend,
@@ -11,15 +13,23 @@ import {
     getAllPendingFriends
 } from '../../utils/backendCalls';
 import {
-    Grid
+    Grid,
+    Button,
+    TextField,
+    Modal
 } from '@mui/material';
+import { Box } from "@mui/system";
 import PersonIcon from '@mui/icons-material/Person';
+import CloseIcon from '@mui/icons-material/Close';
 
 export default function Friends() {
     const [ userData, setUserData ] = useState(null);
+    const [ searchResults, setSearchResults ] = useState(null);
     const [ friends, setFriends ] = useState(null);
     const [ pendingFriends, setPendingFriends] = useState(null);
     const [ error, setError ] = useState(null);
+    const [ searchError, setSearchError ] = useState(null);
+    const [ openSearchModal, setOpenSearchModal ] = useState(false);
 
     const user = useSelector((state) => state.user);
 
@@ -49,13 +59,71 @@ export default function Friends() {
     // check logged in, if not redirect
     if (!user) return <Redirect to="/" />
 
+    const searchUsers = async (e) => {
+        e.preventDefault();
+        
+        let searchTerm = e.target[0].value;
+        // input check
+        try {
+            searchTerm = checkString(searchTerm, 'Search Term', false);
+        } catch (e) {
+
+        }
+        // perform search
+        let results;
+        try {
+            results = await searchUsersByName(searchTerm);
+            console.log(results)
+            setSearchResults(results);
+            setOpenSearchModal(true);
+        } catch (e) {
+            if (!e.response || !e.response.data || !e.response.data.error){
+                setSearchError(e.toString());
+                setOpenSearchModal(false);
+                return;
+            }
+            setSearchError(e.response.data.error);
+            setOpenSearchModal(false);
+            return;
+        }
+
+    }
+
+    const removeFriend = async (e) => {
+        e.preventDefault();
+        
+    }
+
     return (
         <div>
+            {/*something is going weird with this modal but I honestly don't understand what*/}
+            <Modal 
+                open={openSearchModal}
+                onClose={() => setOpenSearchModal(false)}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description">
+                <Box>
+                    <Grid container>
+                        <Grid item xs={1}></Grid>
+                        <Grid item xs={10}><h2>Search Results</h2></Grid>
+                        <Grid item xs={1}><CloseIcon aria-label="close modal" onClick={()=> {setOpenSearchModal(false)}} /></Grid>
+                    </Grid>
+                    {searchResults && searchResults.length >0 && 
+                    <Grid container>
+                        {searchResults.map((user) => (<>
+                        <Grid item xs={7}>{user.username}</Grid>
+                        <Grid item xs={5}>Add Friend</Grid></>))}
+                    </Grid>}
+                </Box>
+            </Modal>
             <h1>Friend Management</h1>
-            <h2>Friend Searchbar Here</h2>
+            <form onSubmit={searchUsers}>
+                <TextField id="userSearch" label="Find users to add" />
+                <Button type="submit">Search</Button>
+            </form>
             <Grid container>
-                <Grid item sm={12} md={6}>
-                    <h2>Friends</h2>
+                <Grid item xs={12} md={4}>
+                    <h2>My Friends</h2>
                     {friends && friends.length > 0 &&
                     <Grid container>
                         {friends.map((friend) => (<>
@@ -65,16 +133,27 @@ export default function Friends() {
                     </Grid>}
                     {friends && friends.length <= 0 && <p>No friends to show.</p>}
                 </Grid>
-                <Grid item sm={12} md={6}>
-                    <h2>Pending Friends</h2>
+                <Grid item xs={12} md={4}>
+                    <h2>Requests Received</h2>
                     {pendingFriends && pendingFriends.length > 0 && 
                     <Grid container>
                         {pendingFriends.map((pending) => (<>
-                        <Grid item xs={1}><PersonIcon /></Grid>
+                        {pending.pending_status === 'received' && <><Grid item xs={1}><PersonIcon /></Grid>
                         <Grid item xs={8.5}>{pending.username}</Grid>
-                        <Grid item xs={2.5}>Action</Grid></>))}
+                        <Grid item xs={2.5}>Action</Grid></>}</>))}
                     </Grid>}
-                    {pendingFriends && pendingFriends.length <= 0 && <p>No pending friends to show.</p>}
+                    {pendingFriends && pendingFriends.length <= 0 && <p>No requests received.</p>}
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <h2>Requests Sent</h2>
+                    {pendingFriends && pendingFriends.length > 0 && 
+                    <Grid container>
+                        {pendingFriends.map((pending) => (<>
+                        {pending.pending_status === 'sent' && <><Grid item xs={1}><PersonIcon /></Grid>
+                        <Grid item xs={8.5}>{pending.username}</Grid>
+                        <Grid item xs={2.5}>Action</Grid></>}</>))}
+                    </Grid>}
+                    {pendingFriends && pendingFriends.length <= 0 && <p>No requests sent.</p>}
                 </Grid>
             </Grid>
         </div>
