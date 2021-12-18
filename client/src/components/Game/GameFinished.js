46619c970d6bbeeb0112fe026f0ed112e910c17d
Elias Frieling <eliasfrieling@gmail.com>
2021-12-18 12:57:04 -0500

    Start new game works now

--- client/src/components/Game/GameFinished.js
+++ client/src/components/Game/GameFinished.js
@@ -1,15 +1,27 @@
-import {Button} from '@mui/material';
-import {Link} from 'react-router-dom';
+import { Button } from '@mui/material';
+import { Link } from 'react-router-dom';
 
-export default function GameFinished(props)
-{
-    let score = props.score;
+export default function GameFinished(props) {
+  let score = props.score;
 
-    return(
-        <div>
-            <p>Your final score is {score}</p>
-            <Button component={Link} to="/game" variant="contained" className="landingButton">Start another Game</Button>
-            <Button component={Link} to="/home" variant="contained" className="landingButton">Back to Home</Button>
-        </div>
-    );
+  return (
+    <div>
+      <p>Your final score is {score}</p>
+      <Button
+        variant="contained"
+        className="landingButton"
+        onClick={() => window.location.reload()}
+      >
+        Start another Game
+      </Button>
+      <Button
+        component={Link}
+        to="/home"
+        variant="contained"
+        className="landingButton"
+      >
+        Back to Home
+      </Button>
+    </div>
+  );
 }
