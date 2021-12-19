634acadca43f15e9669b297615fcbd2df610b098
Jack Brummer <jbrummer@stevens.edu>
2021-12-19 14:36:53 -0500

    Working on individual profiles

--- client/src/components/Profile/IndProfile.js
+++ client/src/components/Profile/IndProfile.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 import { 
     getUserById,
@@ -9,7 +10,8 @@ import {
 	removePendingFriend,
 	acceptPendingFriend,
 	getAllFriends,
-	getAllPendingFriends
+	getAllPendingFriends,
+    getUserByName
 } from '../../utils/backendCalls';
 
 import {
@@ -29,57 +31,72 @@ import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import LeaderboardIcon from '@mui/icons-material/Leaderboard';
 import CheckIcon from '@mui/icons-material/Check';
 import { useSelector, useDispatch } from "react-redux";
-import { Redirect, Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
+import Link from 'next/link'
 import { checkString } from "../../utils/inputChecks";
 import { Box } from "@mui/system";
 
 import styles from './Profile.module.css';
+import AuthContainer from "../../AuthContainer";
+
 
 export default function IndProfile(props) {
-    console.log(props.data);
-
+    console.log(props);
+    const router = useRouter();
     const [profileData, setProfileData] = useState(null);
     const [error, setError] = useState(null);
     
     useEffect(() => {
         async function fetchFriendsListById() {
-            let data = {friendsList : [], name : "", highScores : []};
-
-            for (let friend of props.data.friends) {
-                try {
-                    let friendData = await getUserById(friend);
-                    console.log(friendData);
-                    data.friendsList.push(friendData);
-
-                } catch (e) {
-                    console.log(e);
-                }
-            }
-
-            for (let highScore of props.data.high_scores) {
-                data.highScores.push(highScore);
-            }
-            setProfileData(data);
+            try {
+                let userByName;
+                
+                userByName = (props.data) ? await getUserByName(props.data.username) : await getUserByName(props.match.params.name)
+                
+                console.log(userByName);
             
+                let data = {friendsList : [], name : "", highScores : []};
+
+                if (props) {
+                    for (let friend of userByName.friends) {
+                        try {
+                            let friendData = await getUserById(friend);
+                            console.log(friendData);
+                            data.friendsList.push(friendData);
+        
+                        } catch (e) {
+                            console.log(e);
+                        }
+                    }
+                    
+                    for (let highScore of userByName.high_scores) {
+                        data.highScores.push(highScore);
+                    }
+                    data.username = userByName.username;
+                    setProfileData(data);
+                }
+            } catch (e) {
+                console.log(e);
+            }
         }
         fetchFriendsListById();
-    },[props.data]);
+    },[]);
 
-    console.log(profileData);
     if (profileData) {
         return (
+
             <div className={styles.profile}>
                 
-                <h1>{props.data.username}'s Profile</h1>
+                <h1>{profileData.username}'s Profile</h1>
                 
                 <div className={styles.profileList}>
                 <h2>High Scores</h2>
                 {profileData.highScores.length>0 && <Grid container>
-				{profileData.highScores.map((score, i) => (<>
-					<Grid item className={styles.gridRow} xs={1}><SportsScoreIcon /></Grid>
-					<Grid item className={styles.gridRow} xs={11}>{score}</Grid>
-					</>))}
-			    </Grid>}
+                {profileData.highScores.map((score, i) => (<>
+                    <Grid item className={styles.gridRow} xs={1}><SportsScoreIcon /></Grid>
+                    <Grid item className={styles.gridRow} xs={11}>{score}</Grid>
+                    </>))}
+                </Grid>}
                 {profileData.highScores.length <= 0 && <p>No high scores to show.</p>}
                 
                 </div>
@@ -90,7 +107,7 @@ export default function IndProfile(props) {
                     {profileData.friendsList.map(({_id, username}, i) => (
                         <>
                             <Grid item xs={1} className={styles.gridRow}><PersonIcon className={styles.personIcon} /></Grid>
-                            <Grid item xs={8.5} className={styles.gridRow}>{username}</Grid>
+                            <Grid item xs={8.5} className={styles.gridRow}><Link href={'/profile/' + username}>{username}</Link></Grid>
                             <Grid item xs={2.5} className={styles.gridRow}>
     
                             </Grid>
@@ -100,9 +117,10 @@ export default function IndProfile(props) {
                 </div>
     
             </div>
+
         )
     } else {
-        return (<h1>Erorr</h1>)
+        return (<h1>loading</h1>)
     }
     
 
