35e70af9f3d04f2c8cf03b1d149b8bbddea76a83
asengstrom <aengstro@stevens.edu>
2021-12-19 11:39:51 -0500

    resolved friends lb display issue when score -inf

--- client/src/components/FriendLeaderboard/FriendLeaderboard.js
+++ client/src/components/FriendLeaderboard/FriendLeaderboard.js
@@ -73,7 +73,8 @@ export default function FriendLeaderboard (props) {
 					if (i===0) place = 'firstPlace';
 					else if (i===1) place = 'secondPlace';
 					else if (i===2) place = 'thirdPlace';
-					return (<Grid container xs={12} className={`${styles.gridRow}${isSelf ? ` ${styles.currentUser}` : ''}`}>
+					console.log('score', score)
+					return (score !== '-inf' && <Grid container xs={12} className={`${styles.gridRow}${isSelf ? ` ${styles.currentUser}` : ''}`}>
 						<Grid item xs={1} className={styles[place]}>{i<=2 ? <EmojiEventsIcon /> : <LeaderboardIcon />}</Grid>
 						<Grid item xs={1}>{i+1}</Grid>
 						<Grid item xs={5}>{username}</Grid>
