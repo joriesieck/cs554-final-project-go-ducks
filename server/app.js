b8cefeab57242454e904e9943a35a556159c1bbc
Elias Frieling <eliasfrieling@gmail.com>
2021-12-19 20:59:37 -0500

    User security

--- server/app.js
+++ server/app.js
@@ -3,9 +3,9 @@ const cors = require('cors');
 const admin = require('firebase-admin');
 const app = express();
 const configRoutes = require('./routes');
-
 const serviceAccount = require('./cs554-go-ducks-firebase-adminsdk-vfm00-b40f1c37d3.json');
-
+const getUserRoute = '/users/username';
+const getAllUsers = '/users';
 // following firebase documentation and https://dev.to/dingran/next-js-firebase-authentication-and-middleware-for-api-routes-29m1
 
 admin.initializeApp({
@@ -18,6 +18,10 @@ app.use(express.json());
 // authentication middleware
 app.use(async (req, res, next) => {
   try {
+    if (req.url.includes(getUserRoute) || getAllUsers === req.url) {
+      next();
+      return;
+    }
     const result = await admin.auth().verifyIdToken(req.headers.authtoken);
   } catch (e) {
     res
