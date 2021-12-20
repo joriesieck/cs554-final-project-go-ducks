c2f8f3b9aa4b77910ac1fe048713e6f0c45ae40a
asengstrom <aengstro@stevens.edu>
2021-12-19 21:50:28 -0500

    authtoken placement for edit email and username

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -173,8 +173,9 @@ export default function Profile () {
 			try {
 				newUser = await editUserInfo({
 					originalEmail: userData.email,
-					username: newValue
-				}, authToken);
+					username: newValue,
+					authToken: authToken
+				});
 			} catch (e) {
 				if (!e.response || !e.response.data || !e.response.data.error) {
 					setProviderError(`Something went wrong updating your username: ${e.toString()}`);
@@ -350,8 +351,9 @@ export default function Profile () {
 			try {
 				newUser = await editUserInfo({
 					originalEmail: userData.email,
-					newEmail: fieldToUpdate.value
-				}, authToken);
+					newEmail: fieldToUpdate.value,
+					authToken: authToken
+				});
 				await updateEmail(auth.currentUser, fieldToUpdate.value);
 			} catch (e) {
 				setLoginErrors([e.toString()]);
